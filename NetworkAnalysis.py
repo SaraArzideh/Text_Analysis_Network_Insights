0b68f3d9287479dd9f62d08256f1a44d2bad54e4
Sara Arideh <sara.arzideh@gmail.com>
2024-03-06 13:08:07 +0200

    Mapping

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
-additional_filters = set(["is", "the", "and", "to", "of", "in", "for", "on", "with", "as", "by", ])
+additional_filters = set(["is", "the", "and", "to", "of", "in", "for", "on", "with", "as", "by" ])
 
 
 # Co_occurrence_df is the DataFrame with co-occurrence data
@@ -36,8 +36,20 @@ partition = community_louvain.best_partition(G)
 
 # Visualizing the communities
 plt.figure(figsize=(15, 15))
-pos = nx.spring_layout(G, k=0.1)
+pos = nx.spring_layout(G, k=0.15)
 nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40, cmap=plt.cm.RdYlBu, node_color=list(partition.values()))
 nx.draw_networkx_edges(G, pos, alpha=0.5)
 nx.draw_networkx_labels(G, pos, font_size=8)
 plt.show()
+
+# We've analyzed and identified which cluster corresponds to which category in the model
+cluster_to_category = {0: 'user-centric design', 1: 'usability', 2: 'utility', 3: 'considerations', 4: 'sustainability'}
+
+# Mapping each node (keyword) to its category
+keyword_to_category = {node: cluster_to_category[community] for node, community in partition.items()}
+
+# Inspecting or exporting this mapping
+print(keyword_to_category)
+
+# Saving this mapping to a CSV file
+pd.DataFrame.from_dict(keyword_to_category, orient='index').to_csv('keyword_to_category_mapping.csv')
