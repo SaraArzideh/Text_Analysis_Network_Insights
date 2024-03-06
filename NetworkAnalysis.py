e50199f5ff0f87d823502b624d5627f3cd46db49
Sara Arideh <sara.arzideh@gmail.com>
2024-03-06 10:50:41 +0200

    Plot Appearance Improved

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -1,6 +1,13 @@
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+additional_filters = set(["is", "the", "and", "to", "of", "in", "for", "on", "with", "as", "by", ])
+
 
 # Co_occurrence_df is the DataFrame with co-occurrence data
 co_occurrence_df = pd.read_csv('filtered_co_occurrence_data.csv')
@@ -9,11 +16,14 @@ co_occurrence_df = pd.read_csv('filtered_co_occurrence_data.csv')
 G = nx.Graph()
 
 # Adding edges to the graph based on co-occurrences
-for index, row in co_occurrence_df.iterrows():
-    G.add_edge(row['Keyword'], row['Co-occurring Word'], weight=row['Frequency'])
+frequency_threshold = 60
+for _, row in co_occurrence_df.iterrows():
+    if row['Frequency'] >= frequency_threshold:
+        G.add_edge(row['Keyword'], row['Co-occurring Word'], weight=row['Frequency'])
 
 # Visualising the network
 plt.figure(figsize=(15, 15))
-pos = nx.spring_layout(G, k=0.1)  # k regulates the distance between nodes
-nx.draw(G, pos, with_labels=True, font_weight='bold', node_color='skyblue', edge_color='gray')
+pos = nx.spring_layout(G, k=0.2)  # k regulates the distance between nodes
+nx.draw(G, pos, with_labels=True, font_size=8, font_weight='bold', node_size=40, node_color='skyblue', edge_color='gray')
+plt.title("Co-occurrence Network")
 plt.show()
