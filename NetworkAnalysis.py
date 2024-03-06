88467a798e074297a3b2481a1aba757bff145485
Sara Arideh <sara.arzideh@gmail.com>
2024-03-06 12:51:40 +0200

    Communities Identifying Added

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -27,3 +27,17 @@ pos = nx.spring_layout(G, k=0.2)  # k regulates the distance between nodes
 nx.draw(G, pos, with_labels=True, font_size=8, font_weight='bold', node_size=40, node_color='skyblue', edge_color='gray')
 plt.title("Co-occurrence Network")
 plt.show()
+
+# Identifying clusters
+import community as community_louvain
+
+# Detecting communities in the network
+partition = community_louvain.best_partition(G)
+
+# Visualizing the communities
+plt.figure(figsize=(15, 15))
+pos = nx.spring_layout(G, k=0.1)
+nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40, cmap=plt.cm.RdYlBu, node_color=list(partition.values()))
+nx.draw_networkx_edges(G, pos, alpha=0.5)
+nx.draw_networkx_labels(G, pos, font_size=8)
+plt.show()
